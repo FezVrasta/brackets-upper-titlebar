0ca39216e86a84bcc68ee90f780ded245f8ae470
FezVrasta <fez@tekhy.net>
2014-05-21 12:47:28 +0200

    First version...

---
+++ main.js
@@ -0,0 +1,9 @@
+define(function (require, exports, module) {
+
+    // Brackets modules
+    var ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
+
+    // Load CSS
+    ExtensionUtils.loadStyleSheet(module, "styles/darkbar.css");
+
+});
