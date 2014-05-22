6680bc3f72f099314bf87fc602c11c197c979962
FezVrasta <fez@tekhy.net>
2014-05-22 10:22:56 +0200

    Fixed position of titlebar when resized.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* globals $, define, window, brackets */
 define(function (require, exports, module) {
 
     // Brackets modules
@@ -5,5 +6,20 @@ define(function (require, exports, module) {
 
     // Load CSS
     ExtensionUtils.loadStyleSheet(module, "styles/darkbar.css");
+
+    // Move menu bar on left edge of window
+    var $titlebar   = $("#titlebar"),
+        $sidebar    = $("#sidebar");
+
+    $sidebar
+        .find(".horz-resizer")
+        .mousedown(function() {
+            $(window).mousemove(function() {
+                $titlebar.css("margin-left", $sidebar.width() * -1);
+            });
+        })
+        .mouseup(function() {
+            $(window).unbind("mousemove");
+        });
 
 });
